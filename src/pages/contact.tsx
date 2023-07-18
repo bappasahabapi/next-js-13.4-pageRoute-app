6de45b94d7d706397776ecf787b84f4d5f9bbc0f
Bappa Saha <bappasaha161@gmail.com>
2023-07-18 23:08:39 +0600

    [done] file based routing

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>This is Contact</h1>
+            <Link href="/">Back to  home page</Link>
+        </div>
+    );
+};
+
+export default ContactPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is homepage</h1>
+      <Link href="/about">About page</Link>
+      <br />
+      <Link href="/contact">Contact page</Link>
     </div>
   );
 };
